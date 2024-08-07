42a7036cd4226e939461853815dc765131e862d0
Sangeethasankar <sangeethasankar474@gmail.com>
2024-08-07 18:30:13 +0530

    use reducer

--- useReducer/usereducer/src/App.jsx
+++ useReducer/usereducer/src/App.jsx
@@ -1,14 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useReducer } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const initialState ={count:0}
+
+  const reducer=(state,action)=>{
+    switch(action.type){
+      case 'increase': {
+        return {count:state.count +1}
+      }
+      case 'decrease':{
+        return {count:state.count -1}
+      }
+      case 'input':{
+        return {count:action.payload}
+      }
+      default:{
+        return state
+      }
+    }
+     
+
+  }
+
+
+
+  const [state,dispatch]=useReducer(reducer,initialState)
+
+  // const [count, setCount] = useState(0)
 
   return (
     <>
+      <h1>{state.count}</h1>
+      <button onClick={()=>dispatch({type:'increase'})}>Increase</button>
+      <button onClick={()=>dispatch({type:'decrease'})}>Decrease</button>
+
+
+      <input value={state.count}onChange={(e)=>dispatch({type:'input',payload:Number(e.target.value)})} 
       
+      type='number'></input>
     </>
   )
 }
